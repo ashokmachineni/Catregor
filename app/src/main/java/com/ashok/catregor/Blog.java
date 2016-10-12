ec33aedc31da8d1f270077d87c19c91701fc0230
ashokmachineni <ashok5679m@gmail.com>
2016-10-12 11:32:28 -0400

    Initial commit

---
+++ app/src/main/java/com/ashok/catregor/Blog.java
@@ -0,0 +1,50 @@
+package com.ashok.catregor;
+
+/**
+ * Created by ashok on 10/12/16.
+ */
+
+public class Blog {
+    private String title,image,link,category;
+    public Blog(){}
+
+
+    public Blog(String image, String title, String link, String category) {
+        this.image = image;
+        this.title = title;
+        this.link = link;
+        this.category = category;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
